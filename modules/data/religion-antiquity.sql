8355977dfe3208c584fce70457111ecf3122daab
james-e-crandall <james.e.crandall@gmail.com>
2025-04-13 09:54:15 -0500

    More Pantheosns

--- modules/data/religion-antiquity.sql
+++ modules/data/religion-antiquity.sql
@@ -5,6 +5,28 @@ CREATE TRIGGER OnTypeInsert AFTER INSERT ON Types BEGIN UPDATE Types SET Hash = 
 
 -- One Copy
 
+-- SELECT "_rowid_",* FROM "main"."Criteria" LIMIT 49999 OFFSET 0;
+
+
+-- SELECT *
+-- FROM Criteria
+-- JOIN Criterion on Criterion.CriteriaRowId = Criteria.CriteriaRowId
+
+
+-- SELECT "_rowid_",* FROM "main"."Criterion" WHERE "CriteriaRowId" LIKE '%798%' ESCAPE '\' AND "CriterionRowId" LIKE '%1238%' ESCAPE '\' LIMIT 49999 OFFSET 0;
+
+
+-- SELECT "_rowid_",* FROM "main"."Criteria" WHERE "CriteriaRowId" LIKE '%798%' ESCAPE '\' LIMIT 49999 OFFSET 0;
+
+
+-- UPDATE ProgressionTreeNodeUnlocks
+-- SET TargetType="EFFECT_ADD_BELIEF"
+-- WHERE ProgressionTreeNodeType="NODE_CIVIC_AQ_MAIN_MYSTICISM"
+-- AND TargetKind="KIND_MODIFIER" 
+-- AND TargetType="MOD_PANTHEON_UNLOCK";
+
+
+
 INSERT INTO Types
 (Type, Kind)
 SELECT BeliefType || "_1", "KIND_BELIEF"
@@ -25,7 +47,7 @@ INSERT INTO Belief_Priorities
 SELECT BeliefType || "_1", ListType
 FROM Belief_Priorities;
 
--- Two Copy
+--Two Copy
 
 INSERT INTO Types
 (Type, Kind)
@@ -46,6 +68,8 @@ INSERT INTO Belief_Priorities
 (BeliefType, ListType)
 SELECT BeliefType || "_2", ListType
 FROM Belief_Priorities;
+
+
 
 
 
